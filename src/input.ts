2e687e4be6a74718050fab49033a856f01c25f4e
Geoff Shannon <geoffpshannon@gmail.com>
2020-07-05 19:21:39 -0700

    Add an input for passing additional arguments to cargo audit call

--- src/input.ts
+++ src/input.ts
@@ -4,13 +4,17 @@
 
 import { input } from '@actions-rs/core';
 
+import stringArgv from "string-argv";
+
 // Parsed action input
 export interface Input {
     token: string;
+    args: string[];
 }
 
 export function get(): Input {
     return {
         token: input.getInput('token', { required: true }),
+        args: stringArgv(input.getInput('args')),
     };
 }
