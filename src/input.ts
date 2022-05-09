13eb0e349ae5394e35e9ab5c739d572a161a718f
moliva <oliva.miguelh@gmail.com>
2022-05-09 17:56:25 -0300

    feat: adding optional ignore list

--- src/input.ts
+++ src/input.ts
@@ -7,10 +7,12 @@ import { input } from '@actions-rs/core';
 // Parsed action input
 export interface Input {
     token: string;
+    ignore: string[];
 }
 
 export function get(): Input {
     return {
         token: input.getInput('token', { required: true }),
+        ignore: input.getInputList('ignore', { required: false }),
     };
 }
